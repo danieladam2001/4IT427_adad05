2b8466a7bbf33fd74d91f79c6ab9edecb6093a0d
Daniel Adam <danadam2001@seznam.cz>
2026-05-22 13:09:11 +0200

    feat: Přepínání režimu světlý/tmavý

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { FilmProvider } from './context/WatchlistContext.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FilmProvider>
-      <App />
-    </FilmProvider>
+    <BrowserRouter>
+      <FilmProvider>
+        <App />
+      </FilmProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
