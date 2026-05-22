3bc84ed4c744ac1984bb0233947ad39133ae068e
Daniel Adam <danadam2001@seznam.cz>
2026-05-22 15:48:47 +0200

    chore: Aktualizované balíčky a config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 // vite.config.ts (vznikne při instalaci)
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Volitelne: alias pro importy
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test-setup.ts',
+  },
   resolve: {
     alias: { '@': '/src' },
   },
